e31d8e3f1e9d52048d5339a300f1348853874de3
Cris Butuceanu <161909929+cbutuceanu@users.noreply.github.com>
2025-02-18 15:27:03 -0500

    Added Headbob effect and pause screen layout

--- Abandoned Lab/Assets/Scripts/Player/FlashLight.cs
+++ Abandoned Lab/Assets/Scripts/Player/FlashLight.cs
@@ -5,18 +5,18 @@ using System.Collections.Generic;
 public class Flashlight : MonoBehaviour
 {
     Light light;
+    public PauseMenu pauseMenu; // Reference to the PauseMenu script
 
     void Start()
     {
-
         light = GetComponent<Light>();
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.F))
+        // Only allow flashlight toggle if the game is not paused
+        if (!pauseMenu.isPaused && Input.GetKeyUp(KeyCode.F))
         {
-
             light.enabled = !light.enabled;
         }
     }
