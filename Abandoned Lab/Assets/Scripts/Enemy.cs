05c400fb0dc44555af9bbad366f6bd3c3eb61dbc
Megan Nowik <megannowik@gmail.com>
2025-02-18 18:39:16 -0500

    new assets yippie

--- Abandoned Lab/Assets/Scripts/Enemy.cs
+++ Abandoned Lab/Assets/Scripts/Enemy.cs
@@ -1,46 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public float distance;
-    public Transform Player;
-    public UnityEngine.AI.NavMeshAgent navMeshAgent;
     public Transform[] patrolPoints;
     public int targetPoint;
     public float speed;
+    private GameObject player;
+    private FieldOfView chase;
+    private NavMeshAgent enemy;
 
     // Start is called before the first frame update
     void Start()
     {
         targetPoint = 0;
+        chase = GetComponent<FieldOfView>();
+        enemy = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Target");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position == patrolPoints[targetPoint].position)
+        if (chase.canSeePlayer)
+        {
+            Chase();
+        } 
+        else if (enemy.remainingDistance <= enemy.stoppingDistance)
         {
             increaseTargetInt();
         }
-        transform.position = Vector3.MoveTowards(transform.position , patrolPoints[targetPoint].position, speed * Time.deltaTime);
-
-        distance = Vector3.Distance(this.transform.position , Player.position);
-
-        if(distance < 10)
-        {
-            navMeshAgent.destination = Player.position;
-        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
     }
 
     void increaseTargetInt()
     {
-        targetPoint++;
-
-        if(targetPoint >= patrolPoints.Length)
+        enemy.SetDestination(patrolPoints[targetPoint].position);
+        
+        if(targetPoint <= patrolPoints.Length)
+        {
+            targetPoint++;
+            
+        }
+        if (targetPoint == 11)
         {
             targetPoint = 0;
         }
     }
+
+    void Chase()
+    {
+        enemy.destination = player.transform.position;
+    }
 }
+
